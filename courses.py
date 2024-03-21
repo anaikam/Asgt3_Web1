aba48297f70088872d946c6abbd1f681d477f868
Anaika Mehra <anaikam@princeton.edu>
2024-03-20 21:55:56 -0400

    both

--- courses.py
+++ courses.py
@@ -8,11 +8,11 @@ import flask
 # import reg.sqlite
 #-----------------------------------------------------------------------
 # ask about the folder
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
-@app.route('/searchpage', methods=['GET'])
-def searchpage():
-    html_code = flask.render_template('searchpage.html')
+@app.route('/index', methods=['GET'])
+def index():
+    html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
     return response
