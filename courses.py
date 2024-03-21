4103d2c460953843699f93b302007bdf72d13579
vh8154 <144638698+vh8154@users.noreply.github.com>
2024-03-20 22:49:01 -0400

    searc

--- courses.py
+++ courses.py
@@ -13,7 +13,7 @@ app = flask.Flask(__name__, template_folder='.')
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    html_code = flask.render_template('index.html')
+    html_code = flask.render_template('searchpage.html')
     response = flask.make_response(html_code)
     #database handling here
     return response
