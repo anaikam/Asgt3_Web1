5b819d416d37698fd1547fa8c0f2e26f8de0c103
vh8154 <144638698+vh8154@users.noreply.github.com>
2024-03-24 15:23:23 -0400

    commit

--- courses.py
+++ courses.py
@@ -56,7 +56,7 @@ def regdetails():
     num = flask.request.cookies.get('prev_num')
     area = flask.request.cookies.get('prev_area')
     title = flask.request.cookies.get('prev_title')
-    print(type(classid))
+
     if classid in ('', None):
         html_code = flask.render_template('error.html',
             message = "missing classid")
@@ -68,28 +68,28 @@ def regdetails():
             message = "non-integer classid")
         response = flask.make_response(html_code)
         return response
-    else:
-        try:
-            gen_table, prof_table, dept_table = class_details(classid)
-            department_table = []
-            for row3 in dept_table:
-                if gen_table[0][0] == row3[0]:
-                    department_table.append([row3[1], row3[2]])
-            professors_table = []
-            for row2 in prof_table:
-                if gen_table[0][0] == row2[0]:
-                    professors_table.append(row2[1])
-            html_code = flask.render_template('regdetails.html',
-                classid=classid, general_table=gen_table[0],
-                prof_table=professors_table,
-                dept_table=department_table, dept=dept,
-                area=area, coursenum=num, title=title)
-            response = flask.make_response(html_code)
-            return response
-        except Exception as ex:
-            html_code = flask.render_template('error.html',
-                ex = ex, message = "no class with classid "
-                + classid + " exists")
-            print(str(ex), file=sys.stderr)
-            response = flask.make_response(html_code)
-            return response
+    
+    try:
+        gen_table, prof_table, dept_table = class_details(classid)
+        department_table = []
+        for row3 in dept_table:
+            if gen_table[0][0] == row3[0]:
+                department_table.append([row3[1], row3[2]])
+        professors_table = []
+        for row2 in prof_table:
+            if gen_table[0][0] == row2[0]:
+                professors_table.append(row2[1])
+        html_code = flask.render_template('regdetails.html',
+            classid=classid, general_table=gen_table[0],
+            prof_table=professors_table,
+            dept_table=department_table, dept=dept,
+            area=area, coursenum=num, title=title)
+        response = flask.make_response(html_code)
+        return response
+    except Exception as ex:
+        html_code = flask.render_template('error.html',
+            ex = ex, message = "no class with classid "
+            + classid + " exists")
+        print(str(ex), file=sys.stderr)
+        response = flask.make_response(html_code)
+        return response
