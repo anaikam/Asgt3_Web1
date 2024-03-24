93e837c2c53d0f9550622f39c339cee2b7879ae3
vh8154 <144638698+vh8154@users.noreply.github.com>
2024-03-24 15:16:14 -0400

    commit

--- courses.py
+++ courses.py
@@ -6,7 +6,7 @@
 #-----------------------------------------------------------------------
 import flask
 import database
-from regdetails import class_details 
+from regdetails import class_details
 import sys
 #-----------------------------------------------------------------------
 # ask about the folder
@@ -31,8 +31,9 @@ def index():
             title = ""
         table = database.course_overviews(dept, num, area, title)
     #handle error cases
-        html_code = flask.render_template('searchpage.html', table=table,
-        dept=dept, coursenum=num, area=area, title=title)
+        html_code = flask.render_template('searchpage.html',
+            table=table, dept=dept, coursenum=num, area=area,
+            title=title)
         response = flask.make_response(html_code)
         response.set_cookie('prev_dept', dept)
         response.set_cookie('prev_num', num)
@@ -41,15 +42,15 @@ def index():
     #database handling here
         return response
     except Exception as ex:
-        html_code = flask.render_template('error.html', ex = ex, message = "A server error occurred. Please contact the system administrator.")
+        html_code = flask.render_template('error.html', ex = ex,
+            message = "A server error occurred. "
+            + "Please contact the system administrator.")
         print(str(ex), file=sys.stderr)
         response = flask.make_response(html_code)
         return response
 
 @app.route('/regdetails', methods=['GET'])
 def regdetails():
-    # do a if classid is None check
-    # dont convert to int
     classid = flask.request.args.get('classid')
     dept = flask.request.cookies.get('prev_dept')
     num = flask.request.cookies.get('prev_num')
@@ -57,35 +58,38 @@ def regdetails():
     title = flask.request.cookies.get('prev_title')
     print(type(classid))
     if classid == '' or classid == None:
-        html_code = flask.render_template('error.html', message = "missing classid")
+        html_code = flask.render_template('error.html',
+            message = "missing classid")
         response = flask.make_response(html_code)
         return response
     
     elif classid.isdigit()== False:
-        html_code = flask.render_template('error.html', message = "non-integer classid")
+        html_code = flask.render_template('error.html',
+            message = "non-integer classid")
         response = flask.make_response(html_code)
         return response
     else:
         try: 
-            general_table, prof_table, dept_table = class_details(classid)
+            gen_table, prof_table, dept_table = class_details(classid)
             department_table = []
             for row3 in dept_table:
-                        if general_table[0][0] == row3[0]:
+                        if gen_table[0][0] == row3[0]:
                             department_table.append([row3[1], row3[2]])
             professors_table = []
             for row2 in prof_table:
-                        if general_table[0][0] == row2[0]:
+                        if gen_table[0][0] == row2[0]:
                             professors_table.append(row2[1])
-            html_code = flask.render_template('regdetails.html', classid=classid, general_table=general_table[0], prof_table=professors_table,dept_table=department_table, dept=dept, area=area, coursenum=num, title=title)
+            html_code = flask.render_template('regdetails.html',
+                classid=classid, general_table=gen_table[0],
+                prof_table=professors_table,
+                dept_table=department_table, dept=dept,
+                area=area, coursenum=num, title=title)
             response = flask.make_response(html_code)
             return response
         except Exception as ex:
-            html_code = flask.render_template('error.html', ex = ex, message = "no class with classid "+classid+" exists")
+            html_code = flask.render_template('error.html',
+                ex = ex, message = "no class with classid "
+                + classid + " exists")
             print(str(ex), file=sys.stderr)
             response = flask.make_response(html_code)
             return response
-
-# @app.route('/errors', methods=['GET'])
-# def errors():
-
-    
